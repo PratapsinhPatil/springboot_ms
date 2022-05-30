8499ef93ca5855dc35dc5aa10a6bb5ea54e4b757
PratapsinhPatil <pratapsinh.patil87@gmail.com>
2022-05-30 20:52:12 +0530

    initial commit

---
+++ user-service/src/main/java/com/user/UserController.java
@@ -0,0 +1,63 @@
+package com.user;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	UserService service;
+
+	@ResponseStatus(code = HttpStatus.CREATED)
+	@PostMapping("user")
+	public void createUser(@RequestBody UserVo userVo) {
+		System.out.println("Creating User");
+		service.createUser(userVo);
+		System.out.println("User Created");
+	}
+
+	@GetMapping({ "/v1/user/{id}", "/v1/user" })
+	public List<UserVo> getUsers(@PathVariable(required = false) Optional<Integer> id) {
+		System.out.println("getting Users");
+		return id.isPresent() && id.get() > 0 ? service.getUser(Arrays.asList(id.get())) : service.getUsers();
+	}
+
+	@DeleteMapping("/v1/user/{id}")
+	public void deleteUser(@PathVariable Integer id) {
+		System.out.println("deleting Users");
+		service.deleteUser(id);
+		System.out.println("deleted Users");
+
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public Map<String, String> handleException(MethodArgumentNotValidException exct) {
+		Map<String, String> errorMessages = new HashMap<>();
+		exct.getAllErrors().forEach(error -> {
+			String field = ((FieldError) error).getField();
+			String message = ((FieldError) error).getDefaultMessage();
+			errorMessages.put(field, message);
+		});
+		return errorMessages;
+	}
+}
---
+++ user-service/src/main/java/com/user/UserVo.java
@@ -0,0 +1,44 @@
+package com.user;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class UserVo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	int id;
+	@NotBlank(message = "Name should not be null")
+	String name;
+	@Min(value = 1, message = "value should be > 1")
+	int age;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
